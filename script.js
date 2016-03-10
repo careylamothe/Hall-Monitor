4596da0636bf0f7dbe620dcfd0a65f6dad933b13
Carey LaMothe <carey.lamothe@gmail.com>
2016-03-10 14:13:27 -0800

    fixed some bugs

--- script.js
+++ script.js
@@ -1,55 +1,68 @@
 $(document).ready(function () {
-  var dataRef = new Firebase('https://hall-monitor.firebaseio.com/log/');
+  var dataRef = new Firebase('https://hall-monitor.firebaseio.com/logs/');
   //var $orcasInput = $('select.#orcasInput').val();
-  var count = 0
-  var checkedIn = false
-  var time
+  var count = 0;
+  var status = 'checked out';
+  var time;
 
 //when classroom button is clicked, show child list.
   $('#eagle-button').on('click', function() {
-    $('#eaglesInput').show();
-  })
+    $('#eaglesStudent').show();
+    var $eaglesStudent = $('#eaglesStudent');
+    var $guardian = $('#guardian');
+    var $message = $('#message');
+
 //When the grown-up field is entered, show checkIn button if the value is false.
-  $('#guardian').keypress(function (e) {
-     if (e.keyCode == 13) {
-       if (checkedIn) {
-         ('checkIn').show()
+    $('#guardian').keypress(function (e) {
+       if (e.keyCode == 13) {
+         if (status === "checked out") {
+           $('#checkIn').show();
+
+           //When the check-in button is clicked, get input log data and push to Firebase
+           $('.check').on('click', function (e) {
+             time = Date.now();
+             counter();
+             status = 'checked in';
+
+             dataRef.push({
+               classCount: count,
+               student: $eaglesStudent.val(),
+               guardian: $guardian.val(),
+               status: status,
+               timeStamp: time,
+               message: $message.val()
+             });
+
+             $guardian.val('');
+           })
+
+           //retrieve new items from the database and display
+           dataRef.on('child_added', function(snapshot) {
+             var log = snapshot.val();
+             displayLog(
+               log.classCount,
+               log.student,
+               log.guardian,
+               log.timeStamp,
+               log.message
+             );
+           })
+         }
        }
-     }
-   })
-
-//When the check-in button is clicked, get input log data and push to Firebase
-  $('.check').on('click', function (e) {
-    var $eaglesStudent = $('select.#eaglesStudent').val();
-    var $guardian = $('#guardian').val();
-    var message = $('#message').val();
-
-    time = Date.now();
-    counter();
-    checkedIn = true;
-
-    dataRef.push({classCount: count, student: $eaglesStudent, guardian: $guardian, checkedIn: checkedIn, timeStamp: time, message: message});
-    $guardian.val('');
-  })
-
-//retrieve new items from the database and display
-  dataRef.on('child_added', function(snapshot) {
-    var log = snapshot.val();
-    displayLog(log.classCount, log.student, log.guardian, log.timeStamp, log.message);
-  })
-
+     })
+    })
 //function definitions
   function counter() {
-    if (checkedIn) {
+    if (status === "checked in") {
       count += 1;
     }
-      else {
-        count -= 1;
-      }
+    else {
+      count -= 1;
+    }
   }
-  function displayLog(count, $eaglesStudent, $guardian, time, message) {
+  function displayLog(count, student, guardian, time, message) {
     $('#count').append(count);
-    $('#inData').append($eagelesStudent + ' ' + $guardian + ' ' + time + ' ' + message);
+    $('#inData').append(student + ' ' + guardian + ' ' + time + ' ' + message);
   }
 
 });
