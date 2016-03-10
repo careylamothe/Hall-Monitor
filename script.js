308c5e36ed81d27ba8656ac76098d41cedd61f05
Carey LaMothe <carey.lamothe@gmail.com>
2016-03-10 12:06:32 -0800

    added script

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-
 $(document).ready(function () {
-  var myDataRef = new Firebase('https://hall-monitor.firebaseio.com/');
+  var dataRef = new Firebase('https://hall-monitor.firebaseio.com/log/');
   //var $orcasInput = $('select.#orcasInput').val();
   var count = 0
   var checkedIn = false
   var time
-  var counter = fucntion() {
+
+//when classroom button is clicked, show child list.
+  $('#eagle-button').on('click', function() {
+    $('#eaglesInput').show();
+  })
+//When the grown-up field is entered, show checkIn button if the value is false.
+  $('#guardian').keypress(function (e) {
+     if (e.keyCode == 13) {
+       if (checkedIn) {
+         ('checkIn').show()
+       }
+     }
+   })
+
+//When the check-in button is clicked, get input log data and push to Firebase
+  $('.check').on('click', function (e) {
+    var $eaglesStudent = $('select.#eaglesStudent').val();
+    var $guardian = $('#guardian').val();
+    var message = $('#message').val();
+
+    time = Date.now();
+    counter();
+    checkedIn = true;
+
+    dataRef.push({classCount: count, student: $eaglesStudent, guardian: $guardian, checkedIn: checkedIn, timeStamp: time, message: message});
+    $guardian.val('');
+  })
+
+//retrieve new items from the database and display
+  dataRef.on('child_added', function(snapshot) {
+    var log = snapshot.val();
+    displayLog(log.classCount, log.student, log.guardian, log.timeStamp, log.message);
+  })
+
+//function definitions
+  function counter() {
     if (checkedIn) {
       count += 1;
     }
@@ -13,27 +47,9 @@ $(document).ready(function () {
         count -= 1;
       }
   }
+  function displayLog(count, $eaglesStudent, $guardian, time, message) {
+    $('#count').append(count);
+    $('#inData').append($eagelesStudent + ' ' + $guardian + ' ' + time + ' ' + message);
+  }
 
-  $('#eagle-button').on('click', function() {
-    $('#eaglesInput').show();
-    var $eaglesInput = $('select.#eaglesInput').val();
-    var $guardian = $('#guardian').val();
-    var message = $('#message').val();
-
-    $('#guardian').keypress(function (e) {
-       if (e.keyCode == 13) {
-         if checkedIn {
-           ('checkIn').show()
-         }
-       }
-     }
-    $('.check').on('click', function (e) {
-      time = Date.now();
-      counter();
-    }
-    myDataRef.push({classCount: count, student: $eaglesInput, guardian: $guardian, checkedIn: checkedIn, message: message});
-        $guardian.val('');
-  })
-
-    // append current classroom data to Attendance Log
-    //  Show full student name, guardian name and time.  Include a node to open message
+});
